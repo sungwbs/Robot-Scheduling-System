f14b05fc3d8f0590df707ba21d6986086c16ac03
최성준 <135031749+sungwbs@users.noreply.github.com>
2025-03-05 00:48:33 +0900

    Add files via upload

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "IoT_Project"
---
+++ src/test/java/com/mookseong/iot_project/IoTProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mookseong.iot_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IoTProjectApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
